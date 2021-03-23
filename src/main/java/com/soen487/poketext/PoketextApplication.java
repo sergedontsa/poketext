8605d9cc502b5ed93556c222792acd6c2a1342fe
sergedontsa <63218303+sergedontsa@users.noreply.github.com>
2021-03-23 18:19:38 -0400

    first commit

---
+++ src/main/java/com/soen487/poketext/PoketextApplication.java
@@ -0,0 +1,13 @@
+package com.soen487.poketext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketextApplication.class, args);
+	}
+
+}
