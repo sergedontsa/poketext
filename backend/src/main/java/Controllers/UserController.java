4beb010ef8b0b80f744b9c0ba0d4032a5f3f4c3e
Wilson-La <wilsonla0920@gmail.com>
2021-04-14 15:01:45 -0400

    added skeletons for itemcontroller, pokemoncontroller, and usercontroller

---
+++ backend/src/main/java/Controllers/UserController.java
@@ -0,0 +1,27 @@
+package Controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @PostMapping(value="register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String createUser(@RequestParam("username") String username, @RequestParam("password") String password){
+        return "";
+    }
+
+    @PostMapping(value="login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void login(@RequestParam("username") String username, @RequestParam("password") String password){
+
+    }
+
+    @PostMapping(value="logout", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String logout(@RequestParam("username") String username){
+        return "";
+    }
+}
