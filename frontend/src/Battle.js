3a2bab0dc832569c237bfb94a5c16211e4c821cd
Dominik Ludera <domludera@gmail.com>
2021-04-21 23:35:36 -0400

    donzo-ish

--- frontend/src/Battle.js
+++ frontend/src/Battle.js
@@ -5,6 +5,7 @@ import Charmander from './pictures/F7S8p4xK.jpg';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import axios from 'axios'
+import { useHistory, Redirect } from "react-router-dom";
 
 class Battle extends Component {
 
@@ -30,9 +31,12 @@ class Battle extends Component {
             botMove3: {},
             botMove4: {},
 
-            potionFlag: false
+            potionFlag: false,
+            text_log: '',
+            redirect: false
             //no items for bot
         }
+        this.textLog = ''
         this.data = {}
         this.headers = {
             headers: {
@@ -40,6 +44,19 @@ class Battle extends Component {
             }
         }
 
+    }
+
+    updateTextLog = (newText) => {
+        this.textLog =  this.textLog + newText+ "\n"
+    }
+
+    refreshTextLogState = () => {
+        this.setState({text_log: this.textLog})
+    }
+
+    NewlineText = (props) => {
+        const text = props.text;
+        return text.split('\n').map(str => <p>{str}</p>);
     }
 
     componentDidMount = event => {
@@ -54,7 +71,6 @@ class Battle extends Component {
 
             axios.get('http://localhost:8080/pokemon/0', conf)
                 .then(response => {
-                    console.log(response)
                     this.setState({pokemonSelection: response.data})
                     this.setState({userHp: response.data.hp})
                     return response.data
@@ -62,7 +78,6 @@ class Battle extends Component {
                 .then(response => {
                     axios.get('http://localhost:8080/move/'+this.state.pokemonSelection.name+'/all', conf)
                         .then(response =>{
-                            console.log(response)
                             this.setState({move1: response.data[0]})
                             this.setState({move2: response.data[1]})
                             this.setState({move3: response.data[2]})
@@ -89,17 +104,14 @@ class Battle extends Component {
 
         axios.post('http://localhost:8080/pokemon/'+y, dataBot, headBot)
             .then(response => {
-                console.log(response)
             }).then(response => {
             axios.get('http://localhost:8080/pokemon/0', headBot)
                 .then(response => {
-                    console.log(response.data)
                     this.setState({botSelection: response.data})
                     return response.data
                 }).then(response => {
                 axios.get('http://localhost:8080/move/'+this.state.botSelection.name+'/all', headBot)
                     .then(response =>{
-                        console.log(response)
                         this.setState({botMove1: response.data[0]})
                         this.setState({botMove2: response.data[1]})
                         this.setState({botMove3: response.data[2]})
@@ -131,12 +143,12 @@ class Battle extends Component {
 
         axios.get('http://localhost:8080/item', conf)
             .then(response => {
-                console.log(response.data)
                 attributes = response.data.attributes
-                console.log("ATTRIBUT: " + attributes)
 
                 let b = this.state.pokemonSelection.hp + attributes
                 if (b <= this.state.userHp) {
+                    this.updateTextLog("You used the healing potion")
+                    this.refreshTextLogState()
                     let tempHp = this.state.pokemonSelection
                     tempHp.hp = b
                     this.setState({pokemonSelection: tempHp})
@@ -146,7 +158,6 @@ class Battle extends Component {
     }
 
     botMove = () => {
-        console.log("BOT ATACCKING")
         let botMove = [
             this.state.botMove1,
             this.state.botMove2,
@@ -155,12 +166,13 @@ class Battle extends Component {
         ]
 
         let id = Math.floor(Math.random()*4)
-        console.log("BOT ATTACKING WITH ID: "+id)
         let acc = Math.random(botMove[id].accuracy*1)
         if (acc>botMove[id].accuracy){
             acc = 0
+            this.updateTextLog("The bot has missed")
         }else {
             acc = 1
+            this.updateTextLog("The bot has hit")
         }
         let attack = botMove[id].damage*acc*0.3
         let a = this.state.pokemonSelection.hp - attack
@@ -172,19 +184,31 @@ class Battle extends Component {
         tempState.hp = a
         this.setState({pokemonSelection: tempState })
         if(this.state.pokemonSelection.hp<=0) {
-            alert("GAME OVER! YOU LOST!")
+            alert("MERCI BONSOIR! YOU LOST!")
             axios.put('http://localhost:8080/updateloss', this.data, this.headers)
                 .then(response => {
-                    console.log(response)
                 })
+            this.redirectGameOver()
+
             //redirect
         }
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='/main' />
+        }
+    }
+
+    redirectGameOver = () =>{
+        this.setState({
+            redirect: true
+        })
     }
 
     handleMove = id => event => {
         event.preventDefault()
         // var id = 0
-        console.log("ATTACKING " )
         let pokeMove = [
            this.state.move1,
            this.state.move2,
@@ -196,8 +220,12 @@ class Battle extends Component {
         let acc = Math.random(pokeMove[id].accuracy*1)
         if (acc>pokeMove[id].accuracy){
             acc = 0
+            this.updateTextLog("The attack has missed")
+            //
         }else {
             acc = 1
+            this.updateTextLog("The attack has hit")
+            //
         }
         let attack = pokeMove[id].damage*acc*0.3
         let a = this.state.botSelection.hp - attack
@@ -209,14 +237,15 @@ class Battle extends Component {
         tempState.hp = a
         this.setState({botSelection: tempState })
         if(this.state.botSelection.hp<=0){
-            alert("GAME OVER! YOU WON!")
+            alert("MERCI BONSOIR! YOU WON!")
             axios.put('http://localhost:8080/updatewin', this.data, this.headers)
                 .then(response => {
-                    console.log(response)
                 })
+            this.redirectGameOver()
         }else{
             this.botMove()
         }
+        this.refreshTextLogState()
     }
 
 
@@ -225,6 +254,7 @@ class Battle extends Component {
     render() {
 
         return (
+            <>
             <div className="parentContainer">
                 <div className="usersContainer">
                     {/*USER SECTION*/}
@@ -257,56 +287,13 @@ class Battle extends Component {
 
                 </div>
                 <div className="dialogueContainer">
-                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
-                        or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
-                        thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
-                        specimen book. It usually begins with:
-
-                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.”
-                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph,
-                        page, etc.) that doesn't distract from the layout. A practice not without controversy, laying
-                        out pages with meaningless filler text can be very useful when the focus is meant to be on
-                        design, not content.
-
-                        The passage experienced a surge in popularity during the 1960s when Letraset used it on their
-                        dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their
-                        software. Today it's seen all around the web; on templates, websites, and stock designs. Use our
-                        generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
-                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
-                        or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
-                        thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
-                        specimen book. It usually begins with:
-
-                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.”
-                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph,
-                        page, etc.) that doesn't distract from the layout. A practice not without controversy, laying
-                        out pages with meaningless filler text can be very useful when the focus is meant to be on
-                        design, not content.
-
-                        The passage experienced a surge in popularity during the 1960s when Letraset used it on their
-                        dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their
-                        software. Today it's seen all around the web; on templates, websites, and stock designs. Use our
-                        generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
-                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
-                        or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
-                        thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
-                        specimen book. It usually begins with:
-
-                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.”
-                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph,
-                        page, etc.) that doesn't distract from the layout. A practice not without controversy, laying
-                        out pages with meaningless filler text can be very useful when the focus is meant to be on
-                        design, not content.
-
-                        The passage experienced a surge in popularity during the 1960s when Letraset used it on their
-                        dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their
-                        software. Today it's seen all around the web; on templates, websites, and stock designs. Use our
-                        generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
+                    <this.NewlineText text={this.state.text_log} />
                 </div>
             </div>
+            <div>
+                {this.renderRedirect()}
+            </div>
+            </>
         );
     }
 }
